c5c456e622c08fc97d35476314f7530d74dc00cc
Andy Huntwork <huntwork@amazon.com>
2016-06-11 15:59:21 -0700

    sound-reactor works

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['tst/**/*.js']
+      }
+    },
+    jshint: {
+      options: {
+        strict: false,
+        esversion: 6,
+        predef: [ "-Promise"],
+        node: true
+      },
+      all: [ 'index.js', 'app.js', 'src/**/*.js', 'tst/**/*.js', 'routes/**/*.js' ]
+    },
+    lambda_invoke: {
+        default: {
+            options: {
+                // Task-specific options go here.
+            }
+        }
+    },
+    lambda_package: {
+        default: {
+          options:{
+            include_files: [ 'bin/ffpmeg' ]
+          }
+        },
+        devo: {
+          
+        }
+    }/*,
+    lambda_deploy: {
+        default: {
+            arn: 'arn:aws:lambda:us-east-1:939027667944:function:MagicDoor',
+            options: {
+              enableVersioning: true,
+              aliases: 'dev'
+            }
+        },
+        devo: {
+          arn: 'arn:aws:lambda:us-west-2:939027667944:function:MagicDoor',
+          options: {
+            enableVersioning: true,
+            aliases: 'dev'
+          }
+        }
+    },*/
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-aws-lambda');
+
+  // Default task(s).
+  grunt.registerTask('default', ['mochaTest', 'jshint']);
+  grunt.registerTask('deploy', ['mochaTest', 'jshint', 'lambda_package:default']);
+};
---
+++ index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+let aws = require('aws-sdk');
+let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+let uuid = require('node-uuid');
+let fs = require('fs');
+let zlib = require('zlib');
+let Promise = require('bluebird');
+let execAsync = Promise.promisify(require('child_process').exec);
+
+const FFMPEG = process.env.LAMBDA_TASK_ROOT + "/bin/ffmpeg";
+//const FFMPEG = "/usr/local/bin/ffmpeg";
+
+exports.handler = function(event, context, done) {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const inFile = uuid.v1() + "-" + bucket + "-" + key.replace(/[/]/g, '-');
+    const inPath = '/tmp/' + inFile;
+    const outPath = '/tmp/' + inFile + ".mp3";
+    const outKey = key.replace(/incoming[/]normal[/]/, '').replace(/[.]wav/, '.mp3');
+
+    console.log(`${bucket} ${key} ${inFile} ${inPath} ${outPath} ${outKey}`);
+
+    if(!key.match(/[.](mp3|wav)$/)) {
+    	console.log("nothing to do for " + key);
+    	done();
+    }
+
+	var file = fs.createWriteStream(inPath);
+
+    const params = {
+        Bucket: bucket,
+        Key: key
+    };
+    var stream = s3.getObject(params).createReadStream();
+    stream.pipe(file);
+
+    streamToPromise(stream).then(function() {
+    	console.log("pipe ended");
+
+    	return execAsync(`${FFMPEG} -i ${inPath} -ac 2 -codec:a libmp3lame -b:a 48k -af "volume=15dB" -ar 16000 ${outPath}`);
+
+    }).then(function(stdout, stderr) {
+    	console.log("finished with exec");
+
+		var s3 = new aws.S3({params: {Bucket: bucket, Key: outKey, ACL: "public-read" }});
+		var body = fs.createReadStream(outPath);
+		var upload = s3.upload({Body: body});
+
+		Promise.promisifyAll(Object.getPrototypeOf(upload),{ suffix: 'BBAsync' });
+		return upload.sendBBAsync();
+    }).then(function() {
+    	console.log("finished uploading");
+    	fs.unlinkSync(inPath);
+    	fs.unlinkSync(outPath);
+    }).then(_ => done()).catch(done);
+
+    console.log("done with handler");
+};
+
+function streamToPromise(stream) {
+    return new Promise(function(resolve, reject) {
+        stream.on("end", resolve);
+        stream.on("error", reject);
+    });
+}
+
+if(!module.parent) {
+	exports.handler({
+	  "Records": [
+	    {
+	      "eventVersion": "2.0",
+	      "eventTime": "1970-01-01T00:00:00.000Z",
+	      "requestParameters": {
+	        "sourceIPAddress": "127.0.0.1"
+	      },
+	      "s3": {
+	        "configurationId": "testConfigRule",
+	        "object": {
+	          "eTag": "0123456789abcdef0123456789abcdef",
+	          "sequencer": "0A1B2C3D4E5F678901",
+	          "key": "sounds/incoming/normal/20160611/creek.wav",
+	          "size": 1024
+	        },
+	        "bucket": {
+	          "arn": "arn:aws:s3:::mybucket",
+	          "name": "magicdoor.huntwork.net",
+	          "ownerIdentity": {
+	            "principalId": "EXAMPLE"
+	          }
+	        },
+	        "s3SchemaVersion": "1.0"
+	      },
+	      "responseElements": {
+	        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH",
+	        "x-amz-request-id": "EXAMPLE123456789"
+	      },
+	      "awsRegion": "us-east-1",
+	      "eventName": "ObjectCreated:Put",
+	      "userIdentity": {
+	        "principalId": "EXAMPLE"
+	      },
+	      "eventSource": "aws:s3"
+	    }
+	  ]
+	}, null, e => console.log(e ? ("failed " + JSON.stringify(e)): "succeeded"));
+}
